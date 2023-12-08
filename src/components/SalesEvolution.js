a23fe1ce9f1fecaa4de3e259f5d9dc6e10575930
Razvan4449 <vasiliurazvan4449@gmail.com>
2023-12-08 14:27:16 +0200

    SalesEvo and SalesPred modifications

--- src/components/SalesEvolution.js
+++ src/components/SalesEvolution.js
@@ -2,10 +2,18 @@ import React, { useEffect, useState } from 'react';
 import '../styles/SalesEvolution.css';
 import ajax from '../services/fetchService';
 import Chart from "chart.js/auto";
+import * as FileSaver from 'file-saver';
+import XLSX from 'sheetjs-style';
+
 
 const SalesEvolution = () => {
-  const [salesValues7, setSalesValues7] = useState('');
   const [isCanvasDisplaied, setCanvasDisplaied] = useState(false);
+  const [predictionData, setPredictionData] = useState('');
+  const [months, setMonths] = useState('');
+  const [prediction, setPrediction] = useState('');
+  const [salesValues7, setSalesValues7] = useState('');
+
+  
 
   useEffect(() => {
     ajax("http://localhost:5000/api/form2", "GET")
@@ -15,6 +23,16 @@ const SalesEvolution = () => {
         console.log(e);
       });
   }, [])
+
+  function showPrediction() {
+      ajax("http://localhost:5000/api/form2/prediction?months=" + months, "GET")
+      .then((data) => {
+        setPredictionData(Object.entries(JSON.parse(data.message)))
+      }).catch(e => {
+        console.log(e);
+      });
+  }
+
 
   function showData() {
 
@@ -56,11 +74,46 @@ const SalesEvolution = () => {
     }
   }
 
+  const exportDataWithJsExcel = async () => {
+    const fileName = "Sales_evolution_prediction"
+    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    const fileExtension = '.xlsx';
+
+    const ws = XLSX.utils.json_to_sheet(predictionData);
+
+    const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
+  }
+
+
   return (
     <div className="sales-evolution">
       <h1>Evolution of the sales</h1>
       <div className="chart-container">
         <canvas id="chart"></canvas>
+      </div>
+      <div className="chart-container">
+      {predictionData ? 
+      <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Sales Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {predictionData.map(([date, salesValue], index) => (
+          <tr key={index}>
+            <td>{date}</td>
+            <td>{salesValue}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+      :  ''}
+
       </div>
       <button onClick={showData}>Show</button>
       <div className="data-table">
@@ -70,9 +123,22 @@ const SalesEvolution = () => {
         <div className="trend-type-selector">
           {/* Dropdown to select trend type */}
         </div>
-        <input type="number" placeholder="Enter number of months" />
-        <button>Excel export</button>
-        <button>Predict</button>
+        
+        <input type="number" placeholder="Enter number of months" onChange={(event) => { setMonths(event.target.value) }} />
+        
+        <label htmlFor="prediction-select">Select the prediction:</label>
+        <select id="prediction-select" name="prediction" onChange={(event) => { setPrediction(event.target.value) }}>
+          <option>--Please choose an option--</option>
+          <option value="Logarithmic">Logarithmic</option>
+          <option value="Linear">Linear</option>
+          <option value="Polinomial">Polinomial</option>
+          <option value="Power">Power</option>
+          <option value="Exponential">Exponential</option>
+        </select>
+        
+        <button onClick={exportDataWithJsExcel}>Excel export</button>
+        <button onClick={showPrediction}>Predict</button>
+
       </div>
     </div>
   );
--- src/components/SalesPrediction.js
+++ src/components/SalesPrediction.js
@@ -1,35 +1,132 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import '../styles/SalesPrediction.css';
+import ajax from '../services/fetchService';
+import Chart from "chart.js/auto";
+import * as FileSaver from 'file-saver';
+import XLSX from 'sheetjs-style';
 
 function SalesPrediction() {
-  // Placeholder state and functions (You need to implement these based on your actual data and logic)
-  const genres = ['Rock', 'Pop', 'Jazz', 'HipHop', 'R&B']; // example genres
+  const genres = ['Rock', 'Pop', 'Jazz', 'HipHop', 'R&B'];
+  const [sales, setSales] = useState([]);
+  const chartRef = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
+  const [predictionData, setPredictionData] = useState('');
+  const [prediction, setPrediction] = useState('');
+  const [year, setYear] = useState ('');
+  const [genreSelect, setGenreSelect] = useState('');
+
+
+
   const handleGenreChange = (event) => {
-    // Handle the genre change
+    // Implement genre change handling if needed
   };
 
   const handleYearInput = (event) => {
-    // Handle the year input
-  };
-
-  const handleShowGenreData = (genre) => {
-    // Handle the display of genre data
+    // Implement year input handling if needed
   };
 
   const handlePredict = () => {
-    // Handle the prediction action
+    // Implement prediction action if needed
   };
+  function showPrediction() {
+    const genreIDs = {
+      'Rock': 1,
+      'Pop': 9,
+      'Jazz': 2,
+      'HipHop': 17,
+      'R&B': 14
+    };
+    const genreID = genreIDs[genreSelect];
+
+    ajax(`http://localhost:5000/api/form1/prediction?genreId=${genreID}&year=` + year, "GET")
+    .then((data) => {
+      setPredictionData(data.message);
+    }).catch(e => {
+      console.log(e);
+    });
+}
+
+  const displayChart = (genre) => {
+    const genreIDs = {
+      'Rock': 1,
+      'Pop': 9,
+      'Jazz': 2,
+      'HipHop': 17,
+      'R&B': 14
+    };
+    const genreID = genreIDs[genre];
+
+    ajax(`http://localhost:5000/api/form1?genreId=${genreID}`, "GET")
+      .then((data) => {
+        const newSales = JSON.parse(data.message);
+        setSales(newSales);
+
+        const dates = newSales.map(sale => sale.order_year.toString());
+        const values = newSales.map(sale => sale.avg_sales);
+
+        if (chartRef.current && !chartInstance) {
+          const newChart = new Chart(chartRef.current.getContext('2d'), {
+            type: 'bar',
+            data: {
+              labels: dates,
+              datasets: [{
+                label: '# of Sales',
+                data: values,
+                borderWidth: 1
+              }]
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true
+                }
+              }
+            }
+          });
+          setChartInstance(newChart);
+        } else if (chartInstance) {
+          chartInstance.data.labels = dates;
+          chartInstance.data.datasets[0].data = values;
+          chartInstance.update();
+        }
+      }).catch(e => {
+        console.error(e);
+      });
+  };
+
+  // Render table rows from sales data
+  const renderTableRows = () => {
+    return sales.map((sale, index) => (
+      <tr key={index}>
+        <td>{sale.order_year}</td>
+        <td>{sale.avg_sales}</td>
+      </tr>
+    ));
+  };
+
+  const exportDataWithJsExcel = async () => {
+    const fileName = "Sales_of_events_prediction"
+    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    const fileExtension = '.xlsx';
+
+    const ws = XLSX.utils.json_to_sheet(sales);
+
+    const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
+  }
+
 
   return (
     <div className="sales-prediction-container">
       <div className="chart-and-controls">
         <div className="chart-container">
-          {/* Placeholder for chart component */}
-          <div className="chart-placeholder">Chart will go here</div>
+          <canvas ref={chartRef} id="chart"></canvas>
         </div>
         <div className="controls-container">
           {genres.map((genre) => (
-            <button key={genre} onClick={() => handleShowGenreData(genre)}>
+            <button key={genre} onClick={() => displayChart(genre)}>
               Show {genre}
             </button>
           ))}
@@ -37,24 +134,50 @@ function SalesPrediction() {
       </div>
       <div className="input-and-predict">
         <div className="input-container">
-          <select onChange={handleGenreChange}>
+          <select onChange={(event) =>{
+            setGenreSelect(event.target.value);
+          }}>
             <option value="">Choose genre:</option>
             {genres.map((genre) => (
               <option key={genre} value={genre}>{genre}</option>
             ))}
           </select>
-          <input type="number" placeholder="Enter a year:" onChange={handleYearInput} />
+          <input type="number" placeholder="Enter a year:" onChange={(event)=>{
+            setYear(event.target.value);
+
+          }} />
         </div>
-        <button onClick={handlePredict}>Predict</button>
+        <button onClick={showPrediction}>Predict</button>
       </div>
       <div className="data-display">
-        {/* Placeholder for data table */}
-        <div className="data-table-placeholder">Data table will go here</div>
-        <div className="predicted-value">
-          <label>Predicted value:</label>
-          <input type="text" readOnly />
+        {sales.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Year</th>
+                <th>Average Sales</th>
+              </tr>
+            </thead>
+            <tbody>
+              {renderTableRows()}
+            </tbody>
+          </table>
+        ) : (
+          <p>No sales data to display.</p>
+        )}
+
+        <div className="input-group">
+            <label htmlFor="predicted-value">Predicted value:</label>
+            <input
+              type="text"
+              id="predicted-value"
+              value={predictionData}
+              disabled
+            />
         </div>
       </div>
+      <button onClick={exportDataWithJsExcel}>Excel export</button>
+
     </div>
   );
 }
